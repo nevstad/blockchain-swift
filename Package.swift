be4a8d299f79dd5b5c38157fe609f95297169faf
Magnus Nevstad <magnus.nevstad@eggsdesign.no>
2019-06-26 11:29:51 +0200

    Remove SwiftLog dependency

--- Package.swift
+++ Package.swift
@@ -11,14 +11,13 @@ let package = Package(
             targets: ["BlockchainSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/groue/GRDB.swift.git", from: "4.0.1")
     ],
     targets: [
         .target(
             name: "BlockchainSwift",
-            dependencies: ["Logging", "NIO", "GRDB"]),
+            dependencies: ["NIO", "GRDB"]),
         .testTarget(
             name: "BlockchainSwiftTests",
             dependencies: ["BlockchainSwift"]),
